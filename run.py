24ef3a9f6af788efc7955921dd7f2fed868be08f
promisejohn <promise.john@gmail.com>
2018-06-11 00:44:16 +0800

    full front and back tree

--- run.py
+++ run.py
@@ -1,9 +1,21 @@
 # coding=utf8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from random import *
+from flask_cors import CORS
+
 app = Flask(__name__,
             static_folder = "./dist/static",
             template_folder = "./dist")
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
 
 @app.route('/')
 def index():
